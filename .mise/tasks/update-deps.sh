20566e3ad95ddd9821775234bf41c01113063117
Alexandre ACEBEDO <alexandre@acebedo.fr>
2026-09-13 07:58:37 +0200

    fix: change how printf is used in the scripts

--- .mise/tasks/update-deps.sh
+++ .mise/tasks/update-deps.sh
@@ -9,7 +9,7 @@
 set -euo pipefail
 
 if [ -z "${MISE_TASK_NAME:-}" ]; then
-	printf "\033[31mError: this script must be run via 'mise run <task>' (not executed directly).\033[0m\n" >&2
+	printf "\e[31mError: this script must be run via 'mise run <task>' (not executed directly).\e[0m\n" >&2
 	exit 1
 fi
 
